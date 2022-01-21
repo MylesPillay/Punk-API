725291a9e098d0138cfe38531c87b9713bb54535
4naans <mylespillay@yahoo.co.uk>
2022-01-21 15:48:56 +0000

    still working on stylings

--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -10,26 +10,24 @@ const BeerCard = ({picture, name, tagLine, date, ph, abv, ibu, ebc, description,
   }
 
   const beerCardJSX = (
-
-    <> 
+    <>
     <div className="beerCard__container">
-      <img className="beerCard__picture" src={picture} alt={name} />
-      <h2 className="name">{name}</h2>
-      <h3 className="beerCard__tagLine">{tagLine}</h3>
-      <div className="beerCard__data"></div>
-        <p className="beerCard__date">{date}</p>
-        <p className="beerCard__abv">ABV: {abv}</p>
-        <p className="beerCard__ph">pH: {ph}</p>
-        <p className="beerCard__info">{abv}%</p>
-        <p className="beerCard__info">{ibu}</p>
-        <p className="beerCard__info">{ebc}</p>
+      <div className="beerCard__tile">
+        <img className="beerCard__picture" src={picture} alt={name} />
+        <h2 className="name">{name}</h2>
+        <h3 className="beerCard__tagLine">{tagLine}</h3>
+        <div className="beerCard__data">
+          <p className="beerCard__date">{date}</p>
+          <p className="beerCard__abv">ABV: {abv}</p>
+          <p className="beerCard__ph">pH: {ph}</p>
+          <p className="beerCard__info">{abv}%</p>
+          <p className="beerCard__info">{ibu}</p>
+          <p className="beerCard__info">{ebc}</p>
+        </div>
+      </div>
     </div>
-
-    </>
-  );
-
-  //flipcard to expand extra info
-    
+  </>
+  )
   const expandInfoJSX = (
     <div className="beerCard__extra">
       <h3 className="beerCard__subheading">Food pairings:</h3>
@@ -41,7 +39,7 @@ const BeerCard = ({picture, name, tagLine, date, ph, abv, ibu, ebc, description,
         </ul>
 
         <h3 className="beerCard__description-title">Description:</h3>
-        <p className="cbeerCard__description-text">{description}</p>
+        <p className="beerCard__description-text">{description}</p>
       </div>
     )
         
