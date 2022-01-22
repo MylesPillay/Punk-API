131c046d6fe40531ded7c578e13f1c0786b72388
4naans <mylespillay@yahoo.co.uk>
2022-01-22 12:54:35 +0000

    beer tile layout completed, slight changes to font to be done

--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./BeerCard.scss";
 
-const BeerCard = ({picture, name, tagLine, date, ph, abv, ibu, ebc, description, foodPairing}) => {
+const BeerCard = ({picture, name, tagLine, date, ph, abv, ibu, ebc, description}) => {
   const [expandInfo, setExpandInfo] = useState(false);
 
 
@@ -13,17 +13,20 @@ const BeerCard = ({picture, name, tagLine, date, ph, abv, ibu, ebc, description,
     <>
     <div className="beerCard__container">
       <div className="beerCard__tile">
-        <img className="beerCard__picture" src={picture} alt={name} />
-        <h2 className="beerCard__name">{name}</h2>
-        <h3 className="beerCard__tagLine">{tagLine}</h3>
-        <div className="beerCard__data">
-          <h4 className="data__date">Date:{date}</h4>
-          <h4 className="data">ABV: {abv}%</h4>
-          <h4 className="data">pH: {ph}</h4>
-          <h4 className="data">IBU (bitterness): {ibu}</h4>
-          <h4 className="data">EBC (colour):{ebc}</h4>
+        <div className="beerCard__tile--picture">
+          <img className="beerCard__picture" src={picture} alt={name} />
         </div>
-        <h3>Click for description</h3>
+        <div className="beerCard__tile--information">
+          <h2 className="beerCard__name">{name}</h2>
+          <h3 className="beerCard__tagLine">{tagLine}</h3>
+          <div className="beerCard__data">
+            <h4 className="data__date">Date:{date}</h4>
+            <h4 className="data">ABV: {abv}%</h4>
+            <h4 className="data">pH: {ph}</h4>
+            <h4 className="data">IBU (bitterness): {ibu}</h4>
+            <h4 className="data">EBC (colour):{ebc}</h4>
+          </div>
+          </div>
       </div>
     </div>
   </>
@@ -31,7 +34,7 @@ const BeerCard = ({picture, name, tagLine, date, ph, abv, ibu, ebc, description,
   const expandInfoJSX = (
     <div className="beerCard__extra">
         <h3 className="beerCard__description-title">Description:</h3>
-        <p className="beerCard__description-text">{description}</p>
+        <h5 className="beerCard__description-text">{description}</h5>
       </div>
     )
         
